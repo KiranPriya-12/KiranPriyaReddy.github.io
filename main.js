18a4ff1ee41fe1bb84bdadabb663671442a2fe3a
KiranPriya-12 <102324311+KiranPriya-12@users.noreply.github.com>
2022-03-29 23:18:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let loadImage=(src,callback)=>{
 };
 
 let imagePath=(frameNumber,animation)=>{
-    return "/MartialArts-Game/images/"+animation+"/"+frameNumber+".png";
+    return animation+"/"+frameNumber+".png";
 };
 
 let frames={
